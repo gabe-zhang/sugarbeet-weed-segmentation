9d7cca08ff2a22c602176385706d28771fedf479
Gabriel Zhang <yuanzzhang3@gmail.com>
2025-09-12 22:00:05 +0000

    Ignore config & scripts

--- scripts/predict.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-uv run predict.py \
-    --config /home/zy/proj/weed-detect/semantic_segmentation/config/erfnet_predict.yaml \
-    --ckpt_path /home/zy/proj/weed-detect/semantic_segmentation/models/semantic-seg-erfnet.ckpt \
-    --export_dir /home/zy/proj/weed-detect/semantic_segmentation/prediction/semantics
--- scripts/val.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-uv run val.py \
-    --config /home/zy/proj/weed-detect/semantic_segmentation/config/config_erfnet.yaml \
-    --ckpt_path /home/zy/proj/weed-detect/semantic_segmentation/models/semantic-seg-erfnet.ckpt \
-    --export_dir /home/zy/proj/weed-detect/semantic_segmentation/prediction/semantics
