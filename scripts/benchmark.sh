a1bb5d998ab281b954d781c12ceb9ccea01b562c
Yuan Zhang <yuanzzhang3@gmail.com>
2026-01-16 09:29:53 -0600

    chore: remove benchmark scripts from main

--- scripts/benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Benchmark script for comparing PyTorch vs TensorRT ERFNet models
-# Run on NVIDIA Jetson Xavier NX
-
-uv run src/benchmark.py \
-    --config config/erfnet_predict.yaml \
-    --num_warmup 10 \
-    --num_runs 100
